d92a2bc4ad11539db02d46f9da86cd8fb0cef713
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-06 12:52:26 +0100

    tutte le matrici

--- es1.m
+++ es1.m
@@ -1,10 +1,29 @@
 clc
 clear
-A = [4 3 2 1
-     3 4 3 2
-     2 3 4 3
-     1 2 3 4];
- 
-[T, Q] = metodoQRGivens(hess(A), 1000)
-autovalori = eig(A)
-schur = schur(A)
+
+A1 = [1 1 1 1
+      1 2 2 2
+      1 2 3 3
+      1 2 3 4];
+
+A2 = [15 -2 2
+     1 -10 -3
+     -2 1 0];
+
+A3 = [6 2 3 4
+     7 8 3 4
+     1 2 3 4
+     1 9 0 10];
+
+autovaloriA1 = eig(A1)
+autovaloriA2 = eig(A2)
+autovaloriA3 = eig(A3)
+
+[T, Q] = metodoQRGivens(hess(A1), 30)
+[T, Q] = metodoQRGivens(hess(A1), 60)
+
+[T, Q] = metodoQRGivens(hess(A2), 30)
+[T, Q] = metodoQRGivens(hess(A2), 60)
+
+[T, Q] = metodoQRGivens(hess(A3), 30)
+[T, Q] = metodoQRGivens(hess(A3), 60)
